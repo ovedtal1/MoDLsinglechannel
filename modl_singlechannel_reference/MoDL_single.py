6cf3431b6dcb3dcf2814c33b4cfa45c343d3e6f3
Tal Oved <tal@sipl-tal-ct.ef.technion.ac.il>
2024-06-29 15:57:39 +0000

    Progress

--- modl_singlechannel_reference/MoDL_single.py
+++ modl_singlechannel_reference/MoDL_single.py
@@ -12,6 +12,7 @@ import utils.complex_utils as cplx
 from utils.transforms import SenseModel,SenseModel_single
 from utils.layers3D import ResNet
 from unet.unet_model import UNet
+from unet.unet_model import UNetPrior
 from utils.flare_utils import ConjGrad
 import matplotlib
 # matplotlib.use('TkAgg')
@@ -53,16 +54,17 @@ class UnrolledModel(nn.Module):
         self.share_weights = params.share_weights
         self.modl_lamda = params.modl_lamda
         self.reference_mode = params.reference_mode
+        self.reference_lambda = params.reference_lambda
 
         # Declare ResNets and RNNs for each unrolled iteration
         if self.share_weights:
             print("shared weights")
             self.resnets = nn.ModuleList([UNet(2,2)] * self.num_grad_steps)
-            self.similaritynets = nn.ModuleList([UNet(4,2)] * self.num_grad_steps)
+            self.similaritynets = nn.ModuleList([UNetPrior(4,2)] * self.num_grad_steps)
         else:
             print("No shared weights")
             self.resnets = nn.ModuleList([UNet(2,2) for i in range(self.num_grad_steps)])
-            self.similaritynets = nn.ModuleList([UNet(2,2) for i in range(self.num_grad_steps)])
+            self.similaritynets = nn.ModuleList([UNetPrior(2,2) for i in range(self.num_grad_steps)])
 
         # Declare step sizes for each iteration
 #         init_step_size = torch.tensor([-2.0], dtype=torch.float32).to(params.device)
@@ -115,7 +117,9 @@ class UnrolledModel(nn.Module):
                 #print(combined_input.shape)
                 combined_input = combined_input.permute(0, 3, 1, 2)  # Permute to [batch_size, channels, height, width]
                 refined_image = similaritynet(combined_input)
-                image = refined_image.permute(0, 2, 3, 1)  # Permute back to original shape
+                refined_image = refined_image.permute(0, 2, 3, 1) # Permute back to original shape
+                image = image +  refined_image
+                #image = refined_image.permute(0, 2, 3, 1)
             
 
             rhs = zf_image + self.modl_lamda * image
