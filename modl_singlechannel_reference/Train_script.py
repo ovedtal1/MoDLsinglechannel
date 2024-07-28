40bb33d350ae7aa77649133caf9ed53053e863e1
Tal Oved <tal@sipl-tal-ct.ef.technion.ac.il>
2024-07-28 14:05:11 +0000

    Adding reference fusion

--- modl_singlechannel_reference/Train_script.py
+++ modl_singlechannel_reference/Train_script.py
@@ -33,6 +33,7 @@ from models.Unrolled import Unrolled
 from models.UnrolledRef import UnrolledRef
 from models.UnrolledTransformer import UnrolledTrans
 import matplotlib.pyplot as plt
+from models.UnrolledConvNext import UnrolledConvNext
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -54,35 +55,33 @@ class DataTransform:
         self.rng = np.random.RandomState()
 
     def __call__(self, kspace, target, reference, reference_kspace,slice):
-       
-        im_lowres = abs(sp.ifft(sp.resize(sp.resize(kspace,(172,24)),(172,108))))
+        im_lowres = abs(sp.ifft(sp.resize(sp.resize(kspace,(256,24)),(256,160))))
         magnitude_vals = im_lowres.reshape(-1)
         k = int(round(0.05 * magnitude_vals.shape[0]))
         scale = magnitude_vals[magnitude_vals.argsort()[::-1][k]]
         kspace = kspace/scale
-
+        target = target/scale
         # Convert everything from numpy arrays to tensors
         kspace_torch = cplx.to_tensor(kspace).float()   
-        target_torch = cplx.to_tensor(target).float() / scale
-        
+        target_torch = cplx.to_tensor(target).float()   
         # Use poisson mask instead
-        mask2 = sp.mri.poisson((172,108), 2, calib=(18, 14), dtype=float, crop_corner=False, return_density=True, seed=0, max_attempts=6, tol=0.01)
+        mask2 = sp.mri.poisson((256,160), 5, calib=(18, 14), dtype=float, crop_corner=False, return_density=True, seed=0, max_attempts=6, tol=0.01)
+        mask2[128-10:128+9,80-8:80+7] = 1
         mask_torch = torch.stack([torch.tensor(mask2).float(),torch.tensor(mask2).float()],dim=2)
-    
+        mask_torch = T.kspace_crop(mask_torch,0.67)
         #kspace_torch = T.kspace_cut(mask_torch,0.5)
         kspace_torch = T.awgn_torch(kspace_torch,15,L=1)
         kspace_torch = kspace_torch*mask_torch
 
-    
         ### Reference addition ###
-        im_lowres_ref = abs(sp.ifft(sp.resize(sp.resize(reference_kspace,(172,24)),(172,108))))
+        im_lowres_ref = abs(sp.ifft(sp.resize(sp.resize(reference_kspace,(256,24)),(256,160))))
         magnitude_vals_ref = im_lowres_ref.reshape(-1)
         k_ref = int(round(0.05 * magnitude_vals_ref.shape[0]))
         scale_ref = magnitude_vals_ref[magnitude_vals_ref.argsort()[::-1][k_ref]]
-        reference_torch = cplx.to_tensor(reference).float()/ scale_ref
-        # Resolution degrading
-       
-        return kspace_torch, target_torch,mask_torch, reference_torch
+        reference = reference / scale_ref
+        reference_torch = cplx.to_tensor(reference).float()
+
+        return kspace_torch,target_torch,mask_torch, reference_torch
 
 def create_datasets(args):
     # Generate k-t undersampling masks
@@ -113,36 +112,40 @@ def build_optim(args, params):
 ## Hyper parameters
 params = Namespace()
 params.data_path = "./registered_data/"
-params.batch_size = 16
+params.batch_size = 8
 params.num_grad_steps = 4 #4
 params.num_cg_steps = 8
 params.share_weights = True
 params.modl_lamda = 0.05
-params.lr = 0.001
+params.lr = 0.0001
 params.weight_decay = 0
-params.lr_step_size = 5
-params.lr_gamma = 0.5
-params.epoch = 101
+params.lr_step_size = 20
+params.lr_gamma = 0.4
+params.epoch = 81
 params.reference_mode = 0
 params.reference_lambda = 0.1
 
 train_loader = create_data_loaders(params)
-
 ## Load model and Loss selection
-single_MoDL = UnrolledModel(params).to(device)
+#single_MoDL = UnrolledModel(params).to(device)
+#single_MoDL = UnrolledConvNext(params).to(device)
+#single_MoDL = Unrolled(params).to(device)
+single_MoDL = UnrolledRef(params).to(device)
+#single_MoDL = UnrolledTrans(params).to(device)
 optimizer = build_optim(params, single_MoDL.parameters())
-#scheduler = torch.optim.lr_scheduler.StepLR(optimizer, params.lr_step_size, params.lr_gamma)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=params.lr_gamma, patience=10, verbose=True)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, params.lr_step_size, params.lr_gamma)
+#scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=params.lr_gamma, patience=10, verbose=True)
 criterion = nn.MSELoss()
 #criterion = nn.L1Loss()
 
 epochs_plot = []
 losses_plot = []
+running_loss = 0.
 
 for epoch in range(params.epoch):
     single_MoDL.train()
     avg_loss = 0.
-
+    running_loss = 0.0
     for iter, data in enumerate(train_loader):
         input,target,mask,reference = data
         input = input.to(device)
@@ -152,6 +155,7 @@ for epoch in range(params.epoch):
 
         im_out = single_MoDL(input.float(),reference_image=reference,mask=mask)
         loss = criterion(im_out,target)
+        running_loss = running_loss + loss.item()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -163,7 +167,7 @@ for epoch in range(params.epoch):
             #    f'Loss = {loss.item():.4g} Avg Loss = {avg_loss:.4g}'
             #)
     #Saving the model
-    exp_dir = "L2_checkpoints_poisson_x2_MoDL/"
+    exp_dir = "L2_checkpoints_poisson_x2_SAunrolledRef/"
     if epoch % 10 == 0:
         torch.save(
             {
@@ -175,21 +179,23 @@ for epoch in range(params.epoch):
             },
             f=os.path.join(exp_dir, 'model_%d.pt'%(epoch))
         )
-
-    scheduler.step(loss.item())
+    running_loss = running_loss / len(train_loader)
+    #scheduler.step(running_loss)
+    scheduler.step()
     # Append epoch and average loss to plot lists
     epochs_plot.append(epoch)
-    losses_plot.append(loss.item())
+    losses_plot.append(running_loss)
+
 
 # Plotting the loss curve
 plt.figure()
 plt.plot(epochs_plot, losses_plot, label='Training Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
-plt.title('MoDL L2 train Loss')
+plt.title('SA unrolled with Reference L2 train Loss')
 plt.legend()
 plt.grid(True)
-plt.savefig(os.path.join(exp_dir, 'loss_plot_plato.png'))  # Save plot as an image
+plt.savefig(os.path.join(exp_dir, 'loss_plot_plato_down.png'))  # Save plot as an image
 
 # Save all_losses to a file for later comparison
 losses_file = os.path.join(exp_dir, 'all_losses_plato.txt')
--- modl_singlechannel_reference/models/Unrolled.py
+++ modl_singlechannel_reference/models/Unrolled.py
@@ -16,7 +16,18 @@ from unet.unet_model import UNetPrior
 from utils.flare_utils import ConjGrad
 import matplotlib
 from models.SAmodel import MyNetwork
-
+#from image_fusion import fuse
+from ImageFusionBlock import ImageFusionBlock
+import numpy as np
+from IFCNN import myIFCNN
+## New Try
+from ImageFusion_Dualbranch_Fusion.densefuse_net import DenseFuseNet
+from PIL import Image
+import torchvision.transforms as transforms
+import time
+import os
+from ImageFusion_Dualbranch_Fusion.channel_fusion import channel_f as channel_fusion
+#### End new try
 # matplotlib.use('TkAgg')
 
 class Operator(torch.nn.Module):
@@ -57,12 +68,17 @@ class Unrolled(nn.Module):
         self.modl_lamda = params.modl_lamda
         self.reference_mode = params.reference_mode
         self.reference_lambda = params.reference_lambda
-
+        self.device = 'cuda:0'
+        #self.similarity = ImageFusionBlock().to(self.device)
+        self.similarity = DenseFuseNet().to(self.device)
+        checkpoint = torch.load('./ImageFusion_Dualbranch_Fusion/weights/best.pkl')
+        # checkpoint = torch.load('./train_result/model_weight_new.pkl')
+        self.similarity.load_state_dict(checkpoint['weight'])
         # Declare ResNets and RNNs for each unrolled iteration
         if self.share_weights:
             print("shared weights")
             self.resnets = nn.ModuleList([MyNetwork(2,2)] * self.num_grad_steps)
-            self.similaritynets = nn.ModuleList([UNetPrior(4,2)] * self.num_grad_steps)
+            self.similaritynets = nn.ModuleList([myIFCNN(fuse_scheme=0)] * self.num_grad_steps)
         else:
             print("No shared weights")
             self.resnets = nn.ModuleList([MyNetwork(2,2) for i in range(self.num_grad_steps)])
@@ -101,7 +117,7 @@ class Unrolled(nn.Module):
 #         CG_alg = ConjGrad(Aop_fun=Sense.normal,b=zf_image,verbose=False,l2lam=0.05,max_iter=self.c)
 #         cg_image = CG_alg.forward(zf_image)
 #         pl.ImagePlot(zf_image.detach().cpu())
-        
+        reference_image = reference_image.permute(0,3,1,2) 
 #         sys.exit()
         image = zf_image 
         #reference_image = reference_image.permute(0,3,1,2) 
@@ -126,11 +142,63 @@ class Unrolled(nn.Module):
                 #image = refined_image.permute(0, 2, 3, 1)
             #image = torch.cat([image, reference_image], dim=2) # for transformer
             image = image.permute(0,3,1,2) 
-            image = resnet(kspace=image,reference_image=reference_image,iter=iter)
+            #image = resnet(kspace=image,reference_image=reference_image,iter=iter)
             #image = resnet(image)
-            image = image.permute(0,2,3,1)
-            iter = iter +1
+            
+            if iter < 10:
+                #with torch.no_grad():    
+                real_part = image[:,0,:, :]
+                imag_part = image[:,1,:, :]
+                real_part_ref = reference_image[:,0,:, :]
+                imag_part_ref = reference_image[:,1,:, :]
+                phase = torch.atan2(imag_part, real_part)
+                image_in = torch.sqrt(real_part**2 + imag_part**2).unsqueeze(1)
+                reference_in = torch.sqrt(real_part_ref**2 + imag_part_ref**2).unsqueeze(1)
+                #print(f'in similarity shape: {image_in.shape}')
+                feature1a, feature2a = self.similarity.encoder(image_in,isTest=True),self.similarity.encoder(reference_in,isTest=True)
+                featuresa = channel_fusion(feature1a, feature2a,is_test=True)
+                image_real = self.similarity.decoder(featuresa)
+                #feature1b, feature2b = self.similarity.encoder(imag_part.unsqueeze(1),isTest=True),self.similarity.encoder(imag_part_ref.unsqueeze(1),isTest=True)
+                #featuresb = channel_fusion(feature1b, feature2b,is_test=True)
+                #image_imag = self.similarity.decoder(featuresb)
 
+                    #print(f'out similarity shape: {image.shape}')
+                real_part = image_real[:,0,:,:] * torch.cos(phase)
+                imag_part = image_real[:,0,:,:] * torch.sin(phase)
+                    #print(f'real_part shape: {real_part.shape}')
+                image = torch.stack((real_part, imag_part), dim=1)
+                #print(f'real part shape: {real_part.shape}')
+                #print(f'image shape: {image.shape}')
+            #print(f'image shape after concat {image.shape}')
+            """
+            ## Scans fusion
+            if iter < 2:
+                #ssprint(image.shape)
+                with torch.no_grad():
+                    real_part = image[0,0,:, :]
+                    imag_part = image[0,0,:, :]
+                    phase = torch.atan2(imag_part, real_part)
+                    image_in = image[0,...].permute( 1, 2, 0)
+                    reference_image_in = reference_image[0,...].permute( 1, 2, 0)
+                    #print(f'phase shape {phase.shape}')
+                    #print(f'image size pre similarity: {image_in.shape}')
+                    #print(f'reference size pre similarity: {reference_image_in.shape}')
+                    image = self.similarity(image_in, reference_image_in)
+                    #print(f'image shape after sim {image.shape}')
+                    real_part = image[...,0] * torch.cos(phase)
+                    imag_part = image[...,0] * torch.sin(phase)
+                    image = torch.stack((real_part, imag_part), dim=2)
+                    image = image.permute( 2, 0, 1)
+                    image = image.unsqueeze(0)
+                    #print(f'image shape after concat {image.shape}')
+            """
+            image = resnet(kspace=image,reference_image=reference_image,iter=iter)
+
+            image = image.permute(0,2,3,1)
+            
+            ## data consistency
+            iter = iter +1
+            #if iter < self.num_grad_steps:
             rhs = zf_image + self.modl_lamda * image
             CG_alg = ConjGrad(Aop_fun=Sense.normal,b=rhs,verbose=False,l2lam=self.modl_lamda,max_iter=self.num_cg_steps)
             image = CG_alg.forward(rhs)
